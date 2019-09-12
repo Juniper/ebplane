d51431dbb96a5f3bf4942d16329a8bd13f6f877f
Carlo Contavalli <ccontavalli@juniper.net>
2019-09-12 12:44:50 -0700

    Add bazel support to embed files into binaries directly.
    This is step 2/N to have better toolchains, and embed eBPF
    code into binaries.
    
    The .bzl rule imported allows to link files directly into
    binaries.
    
    Example:
    - You have a "trie_match.o" containing ebpf code.
    - You use cc_embed(name = "trie_match", data = [":trie_match.o"])
    - From your .cc file, you can now include "trie_match.h",
      and use std::string_view trie_match_o to access the raw bytes
      contained in the file.
    
    This should work with separate targets as well, where :trie_match
    is on its own generated by other rules.

---
+++ build/tests/README.md
@@ -0,0 +1,4 @@
+This directory contains tests for the `bazel` rules and configurations themselves.
+
+The goal is to verify that the toolchains, the embedding and ebpf compile rules are
+working as expected.
---
+++ build/tests/embed_test_library.cc
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "build/tests/embed_test.h"
+#include "build/tests/perseverance.h"
+#include "build/tests/mediocrity.h"
+
+#include <string_view>
+
+TEST(EmbedTest, SimpleTest) {
+  EXPECT_EQ(std::string(std::string_view(mediocrity, std::size(mediocrity) - 1)), embedded::mediocrity_txt);
+  EXPECT_EQ(std::string(std::string_view(perseverance, std::size(perseverance) - 1)), embedded::perseverance_txt);
+}
