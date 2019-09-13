cbb0ffcd1c621f36f8376ed09891677b263ef4ac
Carlo Contavalli <ccontavalli@juniper.net>
2019-09-13 13:47:52 -0700

    Update the sample code for ebplane daemon to use the new cc_ebpf target.

--- lib/ebpd.cc
+++ lib/ebpd.cc
@@ -1,2 +1,12 @@
 #include "lib/ebpd.h"
+#include "lib/ebpf/sample.h"
 
+#include <iostream>
+
+// This is just an example function.
+// The ebpf code is available as std::string_view named embedded::sample_pic_o.
+// embedded::sample_pic_o.data() and embedded::sample_pic_o.size() provide
+// access to the bytecode.
+void func(void) {
+  std::cout << embedded::sample_pic_o.size() << std::endl;
+}
