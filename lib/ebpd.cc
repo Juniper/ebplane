adf7bc5dfd6d24ae37332294c0185058215691d7
Carlo Contavalli <ccontavalli@juniper.net>
2019-09-13 15:29:32 -0700

    Minor improvements to the bazel rules just introduced.
    Specifically:
    - Adopt Marcus recommendation to make the namespace configurable.
      This adds some sugar in the ebpf rule specification.
    - Add a few lines of code to strip extensions from filenames.
      This adds some more sugar to ebpf rules.
    - Use the C++ style syntax for ifguards, appending an '_'.
    
    Additionally:
    - update the tests to the new syntax.
    - update the ebpf sample code as well.

--- lib/ebpd.cc
+++ lib/ebpd.cc
@@ -4,9 +4,8 @@
 #include <iostream>
 
 // This is just an example function.
-// The ebpf code is available as std::string_view named embedded::sample_pic_o.
-// embedded::sample_pic_o.data() and embedded::sample_pic_o.size() provide
-// access to the bytecode.
+// The ebpf code is available as std::string_view named ebpf::sample.
+// ebpf::sample.data() and ebpf::sample.size() provide access to the bytecode.
 void func(void) {
-  std::cout << embedded::sample_pic_o.size() << std::endl;
+  std::cout << ebpf::sample.size() << std::endl;
 }
